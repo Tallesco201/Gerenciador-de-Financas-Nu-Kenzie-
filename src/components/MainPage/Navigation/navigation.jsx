9f6348300bb3f13eccec76d0f16616dfdbb78f5d
Talles Simao <102775805+Tallesco201@users.noreply.github.com>
2023-05-17 00:22:44 -0300

    style: Fiz a logica de alteração de cor nos botoes

--- src/components/MainPage/Navigation/navigation.jsx
+++ src/components/MainPage/Navigation/navigation.jsx
@@ -1,19 +1,40 @@
+import { useState } from "react";
 import "./styleNavigation.css";
+
+
+
+
+
 export default function Navigation({ listFilter, setListFilter, toDoList }) {
+
+  const [colorButtonEntrada, setColorButtonEntrada]= useState("btn_Cinza")
+  const [colorButtonDespeza, setColorButtonDespeza]= useState("btn_Cinza")
+  const [colorButtonMostrarTodos, setColorButtonMostrarTodos]= useState("btn_Rosa")
+  
+
+
+  
   function filtrarEntradas() {
     const result = toDoList.filter((itens) => itens.type === "Entrada");
+    setColorButtonEntrada("btn_Rosa")
+    setColorButtonMostrarTodos("btn_Cinza")
+    setColorButtonDespeza("btn_Cinza")
     setListFilter(result);
   }
 
+
   function filtrarDespezas() {
     const result = toDoList.filter((itens) => itens.type === "Despeza");
+    setColorButtonDespeza("btn_Rosa")
+    setColorButtonEntrada("btn_Cinza")
+    setColorButtonMostrarTodos("btn_Cinza")
     setListFilter(result);
     console.log(result);
   }
   function mostrarTodos() {
-    /* const result = toDoList.filter(
-      (item) => item.type === "Despeza" && item.type === "Entrada"
-    ); */
+    setColorButtonMostrarTodos("btn_Rosa")
+    setColorButtonDespeza("btn_Cinza")
+    setColorButtonEntrada("btn_Cinza")
     setListFilter(toDoList);
   }
 
@@ -22,13 +43,13 @@ export default function Navigation({ listFilter, setListFilter, toDoList }) {
       <nav className="containerNavigation">
         <h3>Resumo financeiro</h3>
         <div className="containerBtn">
-          <button className="btn_Rosa" onClick={() => mostrarTodos()}>
+          <button className={colorButtonMostrarTodos} onClick={() => mostrarTodos()}>
             Todos
           </button>
-          <button className="btn_Cinza" onClick={() => filtrarEntradas()}>
+          <button className={colorButtonEntrada} onClick={() => filtrarEntradas()}>
             Entradas
           </button>
-          <button className="btn_Cinza" onClick={() => filtrarDespezas()}>
+          <button className={colorButtonDespeza}  onClick={() => filtrarDespezas()}>
             Despezas
           </button>
         </div>
